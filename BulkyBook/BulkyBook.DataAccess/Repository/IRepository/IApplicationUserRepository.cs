1a5b59bf49ea4767db47e189eb7e00ae60a8faae
Luka Devic <43738975+LukaDevic@users.noreply.github.com>
2020-02-16 17:56:33 +0100

    Added Company and User models, repositories, controllers, views and js files.

---
+++ BulkyBook/BulkyBook.DataAccess/Repository/IRepository/IApplicationUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BulkyBook.Models;
+using BulkyBook.Models.ViewModels;
+
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IApplicationUserRepository : IRepository<ApplicationUser>
+    {
+    }
+}
